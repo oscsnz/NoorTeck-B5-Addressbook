11a3fd395c65d6bc0b280f55166e0f803dc5a84c
oscar <oscar@DESKTOP-1F3E9D2>
2022-01-04 09:34:33 -0800

    3rd commit

--- NoorTeck-B5-Addressbook/src/test/java/com/noorteck/qa/test/AddressBookTest.java
+++ NoorTeck-B5-Addressbook/src/test/java/com/noorteck/qa/test/AddressBookTest.java
@@ -1,13 +1,93 @@
 package com.noorteck.qa.test;
 
+
+import org.testng.annotations.Test;
+
+import org.testng.asserts.SoftAssert;
+
+import com.noorteck.qa.utils.CommonUI;
 import com.noorteck.qa.utils.ObjInitialize;
+
+
 
 public class AddressBookTest extends ObjInitialize {
 
+	
 	public static void main(String[] args) {
 		
+		String url = "http://a.testaddressbook.com/sign_up";
+		CommonUI.openBrowser("chrome");
+		CommonUI.navigate(url);
+		initializeClassObj();
+		
+		
+		testCaseOne();
+		
+				
+		CommonUI.openBrowser("chrome");
+		CommonUI.navigate("http://a.testaddressbook.com/sign_in");
+		testCaseTwo();
+		
+		
+		
+		CommonUI.quitBrowser();
 	}
 	
+	@Test
+	public static void testCaseOne() {
+		
+		SoftAssert softAssert = new SoftAssert();
+		signUpPage.enterEmail("afjadnio@gmail.com");
+		signUpPage.enterPassword("puraTH");
+		signUpPage.clickSignUp();
+		
+		String expTitle = "Welcome to Address Book";
+		String actTitle = signUpPage.getHeaderText();
+		softAssert.assertEquals(expTitle,actTitle);
+		
+		
+		softAssert.assertAll();
+	}
+	@Test
+	public static void testCaseTwo() {
+		
+		
+		SoftAssert softAssert = new SoftAssert();
+		
+		signInPage.enterEmail("Cena.john123@gmail.com");
+		signInPage.enterPassword("John12345");
+		signInPage.clickSignIn();
+		
+		signInPage.clickAddresses();
+	
+		signInPage.clickNewAddress();
+		signInPage.enterFirstName("John");
+		signInPage.enterLastName("Cena");
+		signInPage.enterAddress1("Everywhere");
+		signInPage.enterCity("The world");
+		signInPage.state("text", "New York");
+		signInPage.enterZip("10001");
+		signInPage.country();
+		signInPage.enterAge("22");
+		signInPage.enterPhone("123-2334-3434");
+		signInPage.selectInterest();
+		signInPage.enterNote("Automation is Fun");
+		signInPage.clickCreateAddress();
+		signInPage.clickListLink();
+		String expFName = "John";
+		String expLName = "Cena";
+		String expCity = "The world";
+		String expState = "NY";
+		softAssert.assertEquals(expFName, signInPage.fNameTableResult());
+		softAssert.assertEquals(expLName, signInPage.lNameTableResult());
+		softAssert.assertEquals(expCity, signInPage.cityTableResult());
+		softAssert.assertEquals(expState, signInPage.stateTableResult());
+		
+		
+		softAssert.assertAll();
+		
+		
+	}
 }
 
 
